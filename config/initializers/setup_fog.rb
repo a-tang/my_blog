ec3a540790ea7077896d358d7dcbaf42d6287cc9
Alex Tang <alextang3@outlook.com>
2016-07-28 22:40:40 -0700

    update fog setup

--- config/initializers/setup_fog.rb
+++ config/initializers/setup_fog.rb
@@ -7,6 +7,6 @@ CarrierWave.configure do |config|
     region:                'us-west-2'   # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'blogat'
-  config.fog_public     = false
+  config.fog_public     = true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
 end
